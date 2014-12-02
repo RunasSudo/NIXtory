12d1c97fe109416ff170813c0d312176c0549975
RunasSudo <RunasSudo@gmail.com>
2014-12-02 13:47:30 +1030

    Add interface bits.

--- NIXtory/src/io/github/runassudo/nixtory/NIXtoryWindow.java
+++ NIXtory/src/io/github/runassudo/nixtory/NIXtoryWindow.java
@@ -3,6 +3,13 @@ package io.github.runassudo.nixtory;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class NIXtoryWindow {
 
@@ -43,6 +50,44 @@ public class NIXtoryWindow {
 		frmNixtory.setTitle("NIXtory 0.2 DEV");
 		frmNixtory.setBounds(100, 100, 450, 300);
 		frmNixtory.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTabbedPane tpMain = new JTabbedPane(JTabbedPane.TOP);
+		frmNixtory.getContentPane().add(tpMain, BorderLayout.CENTER);
+		
+		JPanel pnOverview = new JPanel();
+		tpMain.addTab("Overview", null, pnOverview, null);
+		pnOverview.setLayout(new BorderLayout(0, 0));
+		
+		JPanel pnProfiles = new JPanel();
+		pnOverview.add(pnProfiles, BorderLayout.NORTH);
+		pnProfiles.setLayout(new BorderLayout(0, 0));
+		
+		JComboBox cbProfile = new JComboBox();
+		pnProfiles.add(cbProfile, BorderLayout.CENTER);
+		
+		JPanel pnProfileSL = new JPanel();
+		pnProfiles.add(pnProfileSL, BorderLayout.EAST);
+		pnProfileSL.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnProfileSave = new JButton("");
+		btnProfileSave.setIcon(new ImageIcon(NIXtoryWindow.class.getResource("/io/github/runassudo/nixtory/assets/document-save.png")));
+		btnProfileSave.putClientProperty("JButton.buttonType", "segmented");
+		btnProfileSave.putClientProperty("JButton.segmentPosition", "first");
+		pnProfileSL.add(btnProfileSave);
+		
+		JButton btnProfileLoad = new JButton("");
+		btnProfileLoad.setIcon(new ImageIcon(NIXtoryWindow.class.getResource("/io/github/runassudo/nixtory/assets/document-open.png")));
+		btnProfileLoad.putClientProperty("JButton.buttonType", "segmented");
+		btnProfileLoad.putClientProperty("JButton.segmentPosition", "last");
+		pnProfileSL.add(btnProfileLoad);
+		
+		JPanel pnInputs = new JPanel();
+		tpMain.addTab("Input Streams", null, pnInputs, null);
+		
+		JPanel pnOutputs = new JPanel();
+		tpMain.addTab("Output Streams", null, pnOutputs, null);
+		
+		JPanel pnStatus = new JPanel();
+		tpMain.addTab("Status", null, pnStatus, null);
 	}
-
 }
